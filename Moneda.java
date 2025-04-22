65e99dae73b55e4fc0403fb51f8f4c28e6f0b4e3
Joaquín Monsalve <joaquinmonsalve0206@gmail.com>
2025-04-22 15:35:33 -0400

    Add files via upload

---
+++ Moneda.java
@@ -0,0 +1,7 @@
+abstract public class Moneda {
+    public Moneda() {}
+    public Moneda getSerie() {
+        return this;
+    }
+    public abstract int getValor();
+}
---
+++ Moneda1500.java
@@ -0,0 +1,8 @@
+public class Moneda1500 extends Moneda {
+    public Moneda1500() {
+        super();
+    }
+    public int getValor() {
+        return 1500;
+    }
+}
