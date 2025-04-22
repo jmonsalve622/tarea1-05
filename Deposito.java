a72ab4ba9fb28c4612fe47b2e3ecd9cd28fbf986
Joaquín Monsalve <joaquinmonsalve0206@gmail.com>
2025-04-22 16:31:08 -0400

    Add files via upload

---
+++ Deposito.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Deposito <T> {
+    private ArrayList list;
+    public Deposito() {
+        list = new ArrayList();
+    }
+    public void add(T b) {
+        list.add(b);
+    }
+    public T get() {
+        if (list.size() > 0) {
+            return (T) list.remove(0);
+        }
+        else {
+            return null;
+        }
+    }
+    public boolean empty() {
+        if (list.size() == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
