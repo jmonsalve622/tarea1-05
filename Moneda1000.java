ed3bd12e4c053c53657c76f14b2e7ed517a16542
Papayo850 <cristobalvalenzuela8221@gmail.com>
2025-04-26 20:58:04 -0400

    Update Moneda1000.java

--- Moneda1000.java
+++ Moneda1000.java
@@ -1,4 +1,4 @@
-public class Moneda1000 extends Moneda implements Comparable<Moneda>{
+public class Moneda1000 extends Moneda{
     public Moneda1000() {
         super();
     }
@@ -6,13 +6,3 @@ public class Moneda1000 extends Moneda implements Comparable<Moneda>{
         return 1000;
     }
 }
-
-public int compareTo(Moneda o){
-        if (this.getValor() > o.getValor()) {
-            return 1;
-        } else if (this.getValor() < o.getValor()) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
