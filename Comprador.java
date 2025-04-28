cbcbc6acfed52da170e0544d793a2d92735379e6
Papayo850 <cristobalvalenzuela8221@gmail.com>
2025-04-28 12:26:04 -0400

    Update Comprador.java

--- Comprador.java
+++ Comprador.java
@@ -1,3 +1,20 @@
 public class Comprador{
-  
+  private String sonido;
+  private int vuelto;
+
+  public Comprador(Moneda m, int select, Expendedor ex){
+      e= new ex.comprarProducto;
+      vuelto=0;
+      Moneda v;
+      while((v= ex.getVuelto()) != null){
+          vuelto += ex.getVuelto;
+      }
+  }
+  public int cuantoVuelto(){
+      return vuelto;
+  }
+
+  public String queBebiste(){
+      return sonido;
+  }
 }
