2fb02942ac92674c3e63cb1f90197ad1d2bbbcf0
Papayo850 <cristobalvalenzuela8221@gmail.com>
2025-04-29 14:01:15 -0400

    Update Super8.java

--- Super8.java
+++ Super8.java
@@ -2,7 +2,7 @@ public class Super8 extends Dulce {
     public Super8(int s) {
         super(s);
     }
-    public String comer() {
+    public String consumir() {
         return "super8";
     }
 }
