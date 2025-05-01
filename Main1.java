a67f55df4841331e5d11642acdbf84747e97438e
Papayo850 <cristobalvalenzuela8221@gmail.com>
2025-04-30 20:15:42 -0400

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,69 @@
+public class Main1 {
+  public static void main(String[] args) {
+    Expendedor exp = new Expendedor(2);
+    Moneda m=null;
+    Comprador c=null;
+    ArrayList<Moneda> ps = new ArrayList<>();
+    Select s = Selleccion.COCASELECCION;
+    
+    m = new Moneda1000();
+    ps.add(m);                               //Comprar CocaCola con 1000 pesos
+    c = new Comprador(m,s,exp);                                             
+    System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
+
+    s = new Selleccion.FANTASELECCION;
+    m = new Moneda1000();
+    ps.add(m);                               //Comprar Fanta con 1000 pesos
+    c = new Comprador(m,s,exp);
+    System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
+
+    s = new Selleccion.SPRITESELECCION;
+    m = new Moneda1000();
+    ps.add(m);                               //Comprar Sprite con 1000 pesos
+    c = new Comprador(m,s,exp);
+    System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
+
+    s = new Selleccion.SNICKERSSELECCION;
+    m = new Moneda1000();
+    ps.add(m);                               //Comprar Snickers con 1000 pesos
+    c = new Comprador(m,s,exp);
+    System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
+
+    s = new Selleccion.SUPER8SELECCION;
+    m = new Moneda500();
+    ps.add(m);                               //Comprar Super8 con 500 pesos
+    c = new Comprador(m,s,exp);
+    System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
+
+    //m = null;
+    //ps.add(m);                              //este codigo es para probar excepcion "PagoIncorrectoexcepcion"
+    //c = new Comprador(m,s,exp);
+    //System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
+
+
+    //m = new Moneda100();
+    //ps.add(m);                              //este codigo es para probar la excepcion "PagoInsuficienteException"
+    //c = new Comprador(m,s,exp);
+    //System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
+
+    //s = new Selleccion.COCACOLASELECCION;
+    //m = new Moneda1000();
+    //ps.add(m);                              //El deposito de CocaCola tiene 2 bebidad para ofrecer, este es el segundo que da para que la siguiente vez que pida Coca, ya no haya
+    //c = new Comprador(m,s,exp);
+    //System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
+    
+    //m = new Moneda1000();                    
+    //ps.add(m);                              //este codigo es para probar la excepcion "NoHayProductoException"
+    //c = new Comprador(m,s,exp);
+    //System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
+
+    
+    System.out.println(ps);
+
+    Collections.sort(ps);
+    System.out.println(ps);
+
+    Collections.reverse(ps);
+    System.out.println(ps);
+  }
+}
